1dfa8399e78e572a964127911014482f67822913
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2022-12-20 10:20:30 +0700

    environment.prod.ts change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export default {
     production: true,
     appOriginUrl: 'https://e-shopping-with-angular.vercel.app',
-    serverOriginUrl: 'https://e-shopping-api.vercel.app/'
+    serverOriginUrl: 'https://e-shopping-api.vercel.app'
 }
